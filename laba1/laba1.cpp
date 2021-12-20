b9c0cd4c2eeab731d1bb44ea9a6522c7aca05f87
Andrew Ostapenko <quietcoolguy621@gmail.com>
2021-12-20 17:05:07 +0300

    Create laba1.cpp

---
+++ laba1/laba1.cpp
@@ -0,0 +1,24 @@
+﻿#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double x, y, z, s1, s2, rez;
+	cout << setprecision(4) << fixed;
+	setlocale(0, "");
+	cout << "Введите х: ";
+	cin >> x;
+	cout << "Введите y: ";
+	cin >> y;
+	cout << "Введите z: ";
+	cin >> z;
+	s1 = abs(pow(x, y / x) - pow(y / x, 1. / 3.));
+	s2 = (cos(y) - (z / (y - x))) / (1 + pow((y - x), 2));
+	rez = s1 + (s2 * (y - x));
+	cout << "Результат: " << rez << endl;
+	system("pause");
+}
