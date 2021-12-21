cda9846133205863da7149405b810b4d26890c87
Andrew Ostapenko <63357331+Faradenzik@users.noreply.github.com>
2021-12-21 12:17:03 +0300

    Update laba1.cpp

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -7,15 +7,23 @@ int main()
 	double x, y, z, s1, s2, rez;
 	cout << setprecision(4) << fixed;
 	setlocale(0, "");
+	
 	cout << "Введите х: ";
 	cin >> x;
+	
 	cout << "Введите y: ";
 	cin >> y;
+	
 	cout << "Введите z: ";
 	cin >> z;
+	
 	s1 = abs(pow(x, y / x) - pow(y / x, 1. / 3.));
+	
 	s2 = (cos(y) - (z / (y - x))) / (1 + pow((y - x), 2));
+	
 	rez = s1 + (s2 * (y - x));
+	
 	cout << "Результат: " << rez << endl;
+	
 	system("pause");
 }
